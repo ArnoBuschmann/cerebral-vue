07a37183448e62e8ee0f8b6f584834fd34a16a58
Arno Buschmann <info@buschmann-marketing.de>
2017-05-31 11:12:52 +0200

    Cerebral-Vue - lifecycle bug

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <container>
+      <img src="./assets/logo.png">
+      <cerebral></cerebral>
+      <foo></foo>
+    </container>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello';
+
+import Vue from 'vue/dist/vue'
+import {Controller} from 'cerebral'
+import {Container, connect} from 'cerebral/vue'
+import Cerebral from './components/Cerebral'
+import Foo from './components/Foo'
+
+
+const controller = Controller({
+  state: {
+    foo: 'Superduper!',
+    mylist: [{ obst: 'Äpfel' }, { obst: 'Birnen' }, { obst: 'Tomaten' }]
+  }
+})
+
+export default {
+  name: 'app',
+  components: {
+    container: Container(controller),
+    Cerebral,
+    Foo
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Foo.vue
@@ -0,0 +1,49 @@
+<script>
+
+const tmpl = `
+  <div>
+
+    <p>Foo is speaking: {{foo}} Local state says: {{ firstName }}</p>
+    <p>And he has a last name: {{ lastName }}</p>
+
+    <ul>
+      <li v-for="item in mylist">
+        {{ item.obst }}
+      </li>
+    </ul>
+
+  </div>
+`
+
+import Vue from 'vue/dist/vue'
+import {connect} from 'cerebral/vue'
+import {state} from 'cerebral/tags'
+
+export default connect({
+  foo: state`foo`,
+  mylist: state`mylist`
+}, {
+
+  // Local state
+  data() {
+    return {
+      firstName: 'John',
+      lastName: 'Smith'
+    }
+  },
+
+  template: tmpl,
+
+  created: function () {
+    // `this` points to the vm instance
+    console.log('Lifecycle: foo created')
+  },
+
+  // Lifecycle hook
+  beforeMount: function() {
+    console.log('Lifecycle: foo beforeMount')
+  }
+
+})
+
+</script>
